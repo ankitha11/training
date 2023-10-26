e3888d2119f801ddc0ca5ac08822cd269b60fbfb
ankitha <user625@host.docker.internal>
2023-10-26 11:46:48 +0530

    my first

--- devops-demo/src/main/java/com/example/demo/services/WelcomeService.java
+++ devops-demo/src/main/java/com/example/demo/services/WelcomeService.java
@@ -1,13 +1,14 @@
 package com.example.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping
+@Service
 public class WelcomeService {
 	
-	@Autowired
+	
 
 	public String[] getCity() {
 		return new String[] {"Chennai","Pune","Mumbai"};
